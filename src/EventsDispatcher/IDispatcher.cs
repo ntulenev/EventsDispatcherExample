c391735bcad2ff9abaa3f35f40996c0ce8171449
Nikita Tulenev <n1tulenev@gmail.com>
2024-04-14 16:45:46 +0200

    Add IDispatcher

---
+++ src/EventsDispatcher/IDispatcher.cs
@@ -0,0 +1,8 @@
+﻿namespace EventsDispatcher;
+
+public interface IDispatcher
+{
+    public void Subscribe<TEvent>(Func<TEvent, CancellationToken, Task> handler);
+    public void Unsubscribe<TEvent>(Func<TEvent, CancellationToken, Task> handler);
+    public Task PublishAsync<TEvent>(TEvent evt, CancellationToken cancellationToken);
+}
